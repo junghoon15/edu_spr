0f0bb9cc745625b26ef784b3841690630e468560
김정훈 <junghoon15@rsquare.co.kr>
2019-11-06 09:50:45 +0900

    check

---
+++ src/main/kotlin/com/edu/eduspring/EduSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.edu.eduspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EduSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<EduSpringApplication>(*args)
+}
